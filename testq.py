4035c905fe74908540edd2b5c12b5c157af7ac40
sunidhi <dhimann453@gmail.com>
2018-06-13 22:11:54 +0530

    message

---
+++ testq.py
@@ -0,0 +1,29 @@
+#1)#Using print
+print("Hello World")
+
+#2) #joining two strings
+print("Wel Come"+"to"+"python")
+
+#3)#Using input statement
+x=input("Enter the value of x")
+y=input("Enter the value of y")
+z=input("Enter the value of z")
+print("The value of x is",x)
+print("The value of y is",y)
+print("The value of z is",z)
+
+#4) #using print statement
+print("Let’s get started")
+#or
+print('Let\'s get started')
+
+#5)placeholders
+s="Acadview"
+course="Python"
+fees = 5000
+print(('%s'+' giving us a course of '+'%s'+' and its fees '+'%d')%(s,course,fees))
+
+#6)fill in the blanks
+name="sunidhi"
+salary=1000000
+print(('%s''%d')%(name ,salary))
