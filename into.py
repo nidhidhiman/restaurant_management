f6c185444b628a5389dd133791f476d27932f60a
sunidhi <dhimann453@gmail.com>
2018-06-17 18:06:26 +0530

    basics

---
+++ into.py
@@ -0,0 +1,9 @@
+print("sunidhi is my name")
+print("I can't print this stmnt")
+print('I can\'t print this stmnt')
+print("sunidhi "+"Dhiman")
+print ("Ha"*10)
+name = "sunidhi"
+course = "CSE"
+rollno = 6315053
+print(("my name is "+"%s"+" m in b.tech "+"%s"+" branch and my rollno is "+"%d")%(name,course,rollno))
